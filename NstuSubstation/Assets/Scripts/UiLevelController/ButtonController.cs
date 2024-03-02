7ac9eb3fe9e8a560f90c7665b8bfcd392389c804
Dmitry <dima.smorodnikov.107@gmail.com>
2024-03-03 03:39:21 +0700

    Added level controller

---
+++ NstuSubstation/Assets/Scripts/UiLevelController/ButtonController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace UiLevelController
+{
+    public class ButtonController : MonoBehaviour
+    {
+        [SerializeField] private LevelController levelController;
+        public void NextLevel()
+        {
+            levelController.NextLevel();
+        }
+
+        public void PreviousLevel()
+        {
+            levelController.PreviousLevel();
+        }
+
+        public void PlayLevel()
+        {
+            levelController.PlayLevel();
+        }
+    }
+}
---
+++ NstuSubstation/Assets/Scripts/UiLevelController/LevelInfo.cs
@@ -0,0 +1,47 @@
+
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UiLevelController
+{
+    public class LevelInfo : MonoBehaviour
+    {
+        public string levelName = "DefaultName";
+        public Sprite levelSprite;
+        public int levelSceneId;
+        public GameObject levelGameObject;
+        [SerializeField] private TextMeshProUGUI levelNameTmp;
+        [SerializeField] private Image levelImage;
+
+
+        private void Start()
+        {
+            SetVariables();
+        }
+
+        private void SetVariables()
+        {
+            SetTextFromVariable();
+            SetGameObject();
+            SetImageFromVariable();
+        }
+        private void SetGameObject()
+        {
+            if (levelGameObject == null)
+            {
+                levelGameObject = gameObject;
+            }
+        }
+
+        private void SetImageFromVariable()
+        {
+            levelImage.sprite = levelSprite;
+        }
+        private void SetTextFromVariable()
+        {
+            levelNameTmp.text = levelName;
+        }
+    }
+}
