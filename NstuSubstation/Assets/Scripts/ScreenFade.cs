fdf945f8c8a3afb973354e7522487b5717cc5180
gallen4 <imil.gibadulin@list.ru>
2024-03-03 04:49:00 +0700

    Excursion V2 (almost prepared)

---
+++ NstuSubstation/Assets/Scripts/ScreenFade.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFade : MonoBehaviour
+{
+    [SerializeField] private float fadeSpeed = 0f;
+
+    public bool nextButton;
+    public bool prevButton;
+    public ElementObservation element;
+
+    private void Awake() { element = FindObjectOfType<ElementObservation>(); }
+
+    private IEnumerator NextFade()
+    {
+       Image fadeImage = GetComponent<Image>();
+       Color fadeColor = fadeImage.color;
+       element.StopAudio();
+
+       while (fadeColor.a < 1f)
+       {
+           fadeColor.a += fadeSpeed * Time.deltaTime;
+           fadeImage.color = fadeColor;
+           yield return null;
+       }
+       
+       element.NextPoint();
+       element.ChangePlayerPosition();
+       
+       while (fadeColor.a >= 0f) 
+       {
+           fadeColor.a -= fadeSpeed * Time.deltaTime;
+           fadeImage.color = fadeColor;
+           yield return null;
+       }
+
+       gameObject.SetActive(false);
+    }
+    
+    private IEnumerator PrevFade()
+    {
+        Image fadeImage = GetComponent<Image>();
+        Color fadeColor = fadeImage.color;
+        element.StopAudio();
+
+        while (fadeColor.a < 1f)
+        {
+            fadeColor.a += fadeSpeed * Time.deltaTime;
+            fadeImage.color = fadeColor;
+            yield return null;
+        }
+        
+        element.PreviousPoint();
+        element.ChangePlayerPosition();
+        
+        while (fadeColor.a >= 0f) 
+        {
+            fadeColor.a -= fadeSpeed * Time.deltaTime;
+            fadeImage.color = fadeColor;
+            yield return null;
+        }
+
+        gameObject.SetActive(false);
+    }
+
+    public void OnNextButton()
+    {
+        nextButton = true;
+        prevButton = false;
+    }
+
+    public void OnPrevButton()
+    {
+        prevButton = true;
+        nextButton = false;
+    }
+    
+    private void OnEnable()
+    {
+        if(nextButton == true)
+            StartCoroutine("NextFade");
+
+        if (prevButton == true)
+            StartCoroutine("PrevFade");
+    }
+}
