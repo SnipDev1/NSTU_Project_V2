7ba3ffe3674f4382e1f350fd381b36828bef2d3f
Dmitry <dima.smorodnikov.107@gmail.com>
2024-03-03 05:01:47 +0700

    Squashed commit of the following:
    commit fdf945f8c8a3afb973354e7522487b5717cc5180
    Author: gallen4 <imil.gibadulin@list.ru>
    Date:   Sun Mar 3 04:49:00 2024 +0700
    
        Excursion V2 (almost prepared)
    
    commit 1d765948ed49ed3e7a0a511efef37d43237b2bc0
    Author: gallen4 <imil.gibadulin@list.ru>
    Date:   Sun Mar 3 02:03:41 2024 +0700
    
        Update Yellow to Orange - Vertical.asset.meta
    
    commit c9bff803e26db024f0e94560c8266415101bf80a
    Author: gallen4 <imil.gibadulin@list.ru>
    Date:   Sun Mar 3 02:02:57 2024 +0700
    
        Update Yellow to Orange - Vertical.asset.meta
    
    commit 437d061e1fa3a2dc4a6c590058c00ce672781d54
    Merge: f866155 54efa71
    Author: gallen4 <imil.gibadulin@list.ru>
    Date:   Sun Mar 3 02:02:52 2024 +0700
    
        Merge branch 'main' into gallenDevelopment
    
    commit f8661551f11c2bace2e34e697db7969faecf2b93
    Author: gallen4 <imil.gibadulin@list.ru>
    Date:   Sat Mar 2 23:51:18 2024 +0700
    
        Excursion V1
    
    commit 7bfa876aa00cfc75709c969b7d514d63e58f5b18
    Author: gallen4 <imil.gibadulin@list.ru>
    Date:   Sat Mar 2 12:45:35 2024 +0700
    
        Outline Added V1

--- NstuSubstation/Assets/Scripts/ElementObservation.cs
+++ NstuSubstation/Assets/Scripts/ElementObservation.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using EPOOutline;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Valve.VR.InteractionSystem;
 
 public class ElementObservation : MonoBehaviour
@@ -19,24 +18,107 @@ public class ElementObservation : MonoBehaviour
         public string elementHeaderText;
         public string elementBodyText;
 
-        public AudioSource elementAudio;    
+        public AudioClip elementAudio;    
     }
     
     public List<Element> kElements = new();
+    
     private int currentPoint = 0;
+    private bool isFirstElement = true;
+    private bool isLastElement = false;
+    private AudioSource audioSource;
+    [SerializeField] private GameObject imageFade;
 
+    private void Start() { audioSource = Player.instance.GetComponent<AudioSource>(); /* Определение аудиосоурса */ }
+
+    private void Update()
+    {
+        if (isFirstElement == false && isLastElement == false)
+        {
+            if (CheckAudioState() == false)
+            {
+                NextPoint();
+            }
+        }
+    }
+    
     public void NextPoint() // Переход к следующей точке осмотра объекта    
      {       
          // ВАЖНО понимать, что из-за этой логики телепорт игрока начинается с 1-й точки, а не с 0-й.
+
+         if (currentPoint == 0)
+             isLastElement = true; // Проверка последний ли элемент (0?)
          
          if (currentPoint < kElements.Count)
              kElements[currentPoint].elementObject.GetComponent<Outlinable>().OutlineParameters.Enabled = false; // Выключение предыдущего OUTLINE объекта
+         isFirstElement = false; // Проверка первый ли элемент (1?)
  
          currentPoint = (currentPoint + 1) % kElements.Count; // Увеличиваем индекс на 1 либо сбрашиваем до 0, если достигнули последнего объекта
-             
+         
          Player.instance.transform.position = kElements[currentPoint].elementObservationPoint.transform.position; // Телепорт к следующей точке
+         Player.instance.transform.LookAt(kElements[currentPoint].elementObject.transform); // Резкий переход камеры на объект ??
+         audioSource.clip = kElements[currentPoint].elementAudio; // Задаем клип аудиосоурсу, получая его с объекта
+         audioSource.Play(); // Проигрыш аудио
          
          kElements[currentPoint].elementObject.GetComponent<Outlinable>().OutlineParameters.Enabled = true; // Включение аутлайна новой точки
-         
-     }   
- }
+     }
+    
+    public void PreviousPoint() // Переход к предыдущей точке осмотра объекта    
+    {       
+        // ВАЖНО понимать, что из-за этой логики телепорт игрока начинается с 1-й точки, а не с 0-й.
+
+        if (currentPoint != 0)
+        {
+            if (currentPoint < kElements.Count)
+                kElements[currentPoint].elementObject.GetComponent<Outlinable>().OutlineParameters.Enabled = false; // Выключение предыдущего OUTLINE объекта
+
+            currentPoint = (currentPoint - 1) % kElements.Count; // Увеличиваем индекс на 1 либо сбрашиваем до 0, если достигнули последнего объекта
+
+            Player.instance.transform.position = kElements[currentPoint].elementObservationPoint.transform.position; // Телепорт к следующей точке
+            Player.instance.transform.LookAt(kElements[currentPoint].elementObject.transform); // Резкий переход камеры на объект ??
+            audioSource.clip = kElements[currentPoint].elementAudio; // Задаем клип аудиосоурсу, получая его с объекта
+            audioSource.Play(); // Проигрыш аудио
+
+            kElements[currentPoint].elementObject.GetComponent<Outlinable>().OutlineParameters.Enabled = true; // Включение аутлайна новой точки
+        }
+    }
+
+    public void ChangePlayerPosition()
+    {
+        Player.instance.transform.position = kElements[currentPoint].elementObservationPoint.transform.position; // Телепорт к следующей точке
+    } // Вызывается в Screenfade.cs
+    
+    public bool CheckAudioState() // Проверка окончания аудио
+    {
+        if (audioSource.isPlaying) return true; // Если аудио играет = TRUE
+        if (!audioSource.isPlaying && (audioSource.time == 0f)) return false; // Если аудио не играет = FALSE
+        
+        return false;
+    }
+
+    public void PauseAudio()
+    {
+        Player.instance.GetComponent<AudioSource>().Pause();
+    } // Пауза аудио
+
+    public void ContinueAudio()
+    {
+        Player.instance.GetComponent<AudioSource>().UnPause();
+    } // Продолжение аудио
+
+    public void ResetAudio() // Начать аудио сначала
+    {
+        audioSource.UnPause();
+        audioSource.Play();
+    }
+
+    public void StopAudio()
+    {
+        audioSource.Stop();
+    } // Стоп аудио
+    
+    public void ExitToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(0);
+    } // Выход в меню (хаб)
+}
