865b64dabed8cb2eb9879a5ab5c8dbe12689f2ad
gallen4 <imil.gibadulin@list.ru>
2024-03-16 01:15:05 +0700

    Pointer Fix #4

--- NstuSubstation/Assets/PointerController.cs
+++ NstuSubstation/Assets/PointerController.cs
@@ -16,9 +16,6 @@ public class PointerController : MonoBehaviour
 
     public void SetRotation()
     {
-        var transformRotation = pointer.transform.rotation;
-        transformRotation.x = 0f;
-        transformRotation.y = 0f;
-        transformRotation.z = 0f;
+        gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
